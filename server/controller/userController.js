0424bc25c1b261078dc7c9a3312305b20401b7c0
Manohar karki <bishalkarki040@gmail.com>
2026-05-16 07:30:41 +0545

    design of register and added jwt token

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -95,6 +95,28 @@ const login = async (req, res) => {
       success: true,
       user,
     });
+
+    const token = jwt.login(
+      {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {expiresIn: "1d"},
+    );
+
+    res.status(200).json({
+      message: "login successfull",
+      success: true,
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+
   } catch (error) {
     return res.status(500).json({
       message: `server error${error}`,
