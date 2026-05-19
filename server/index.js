81bc12438caf02487edddbeb29d0cf816c79ee54
Manohar karki <bishalkarki040@gmail.com>
2026-05-19 07:01:27 +0545

    created auth routes and hit api

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
-const connectToDatabase = require("./config/db")
+const connectToDatabase = require("./config/db");
+const userRoutes = require("./routes/Auth");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", userRoutes)
+
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
 connectToDatabase()
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- server/model/userModel.js
+++ server/model/userModel.js
@@ -6,4 +6,4 @@ const UserSchema = mongoose.Schema({
   password: { required: true, type: String },
 });
 
-module.exports = mongoose.model(User, UserSchema);
+module.exports = mongoose.model("User", UserSchema);
