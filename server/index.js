ac4a4876bb7ad0c4b2bf5c33c5ea881ffcc037cf
Manohar karki <bishalkarki040@gmail.com>
2026-05-21 13:39:33 +0545

    redo-again

--- server/index.js
+++ server/index.js
@@ -6,10 +6,7 @@ const userRoutes = require("./routes/Auth");
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", userRoutes)
