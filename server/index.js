7c889d6e10fcced9f5463b5955a836a282291421
dartSasant <sasantpudasaini@gmail.com>
2026-05-21 11:32:22 +0545

    Added methods

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectToDatabase = require("./config/db");
@@ -6,19 +6,22 @@ const userRoutes = require("./routes/Auth");
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
-app.use("/api", userRoutes)
+app.use("/api", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-connectToDatabase()
+connectToDatabase();
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
